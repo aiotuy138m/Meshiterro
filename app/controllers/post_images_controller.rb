2618db59f2eccb34b98063a2c63aac96d5cc462f
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-01-15 01:51:35 +0000

    migrate修正

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,10 +1,29 @@
 class PostImagesController < ApplicationController
   def new
+    @post_image = PostImages.new
   end
+
+  def create
+    @post_image = PostImages.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_image_path
+  end
+
 
   def index
   end
 
   def show
   end
+  
+  def destroy
+  end
+  
+  private
+  
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caotion)
+  end
+  
 end
