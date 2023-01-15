62a4f4ed75b136d2def287d3fa5d6d1d8e9994c1
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-01-15 13:44:55 +0000

    [Finish]4-12

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -16,15 +16,16 @@ class PostImagesController < ApplicationController
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
   end
-  
+
   def destroy
   end
-  
+
   private
-  
+
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caotion)
   end
-  
+
 end
