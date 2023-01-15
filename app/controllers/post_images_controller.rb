f47fdbd40a872c7c2918c56fdada8d8d3aa6c289
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-01-15 02:05:20 +0000

    [Finish]4-10

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,13 +1,13 @@
 class PostImagesController < ApplicationController
   def new
-    @post_image = PostImages.new
+    @post_image = PostImage.new
   end
 
   def create
-    @post_image = PostImages.new(post_image_params)
+    @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     @post_image.save
-    redirect_to post_image_path
+    redirect_to post_images_path
   end
 
 
